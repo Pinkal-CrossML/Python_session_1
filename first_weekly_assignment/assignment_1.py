f0b66a5673255f3529285c67822ea856e0c33031
Pinkal-CrossML <88371180+Pinkal-CrossML@users.noreply.github.com>
2021-08-10 17:01:07 +0530

    make change with class

--- first_weekly_assignment/assignment_1.py
+++ first_weekly_assignment/assignment_1.py
@@ -1,32 +1,55 @@
-import datetime
-import os
+"""
+Problem:
+Create a shell script that will unzip the user.zip archive and run python script that
+ 1. Display Time and Username.
+ 2. Read each file one by one and display the names of users in each file.
+ 3. Generate a
+    * userId -> random UUID
+    * dummy email address based on user name.
+ 4. store all the data in json file with format:
+
+"""
+from datetime import datetime
 import getpass
-today = datetime.datetime.now()
-
-today_data_time = today.strftime("%d/%m/%y,%H:%M:%S")
-print(today_data_time)
-print(getpass.getuser())
-PWD = os.getcwd()
-users_folder = PWD+'/users/'
+import os
 
 
-def display_names(files_path):
-	files_list = os.listdir((files_path))
+class User_information():
+    """
+    In this function user_datetime_name(),
+    we will Display Time and Username of user.
+    """
+    def user_datetime_name(self):
+        time = datetime.now().time().strftime('%H:%M:%S')
+        username = getpass.getuser()
+        print(f"Time: {time} ,Username: {username}")
 
-	for file_name in files_list:
-		with open(files_path+file_name, 'r') as file:
-			names = file.readlines()
-			for name in names:
-				print(name, end="")
+    """
+    In this function we display the text file
+    and inside data(name) in the form of list 
+
+    """
+
+    def user_name(self):
+        path = "/home/user/crossml/first_weekly_assignment/users/"
+
+        file_name = os.listdir(path)
+        for file in file_name:
+            read_file = open(path + file, 'r')
+            read_data = read_file.readlines()
+            for name in read_data:
+                print(name.strip())
+        read_file.close()
 
 
-def gen_uuid(username):
-	import random
-	uuid = username.split()[0][0].upper()+username.split()[1][0].upper()
-	for _ in range(4):
-		uuid += str(random.randint(1001,9999))
-		uuid += "-"
-	return uuid.strip("-")
+user = User_information()
+user.user_name()
+
+
+user = User_information()
+user.user_datetime_name()
+
+
 
 
 
